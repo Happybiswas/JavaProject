6134b3f714e96037a9b852da50b20c93bde4dc3c
HappyBiswas <happybiswas1983@yahoo.com>
2021-08-25 20:39:13 -0400

    Amazon invoice fie update

--- UdemyProject/src/AmazonOrderListProject/AmazonOrderDetailsSimplified.java
+++ UdemyProject/src/AmazonOrderListProject/AmazonOrderDetailsSimplified.java
@@ -31,9 +31,7 @@ public class AmazonOrderDetailsSimplified {
         amazonOd.PrintOrderDetails(order);
         amazonOd.printInvoice(order);
 
-
     }
-
     public void PrintOrderDetails(Order order) {
 
         System.out.println("**************");
@@ -80,17 +78,16 @@ public class AmazonOrderDetailsSimplified {
 
         return order;
     }
-    //
 
     public ArrayList<OrderItem> createOrderItems(ArrayList<Product> products) {
 
         ArrayList<OrderItem> orderItems = new ArrayList<OrderItem>();
 
-        for (int i = 0; i <= products.size(); i++) {
-            if (i % 2 != 0) {
+        for (int i = 0; i <= products.size()-1; i++) {
+           // if (i % 2 != 0) {
                 orderItems.add(new OrderItem(products.get(i), i));
 
-            }
+          //  }
 
         }
         return orderItems;
@@ -138,7 +135,51 @@ public class AmazonOrderDetailsSimplified {
     }
 
     public void printInvoice(Order order) {
-        System.out.println();
-    }
+
+            System.out.println("\n***********************************************************");
+            System.out.println("Final Invoice Details for Order #" + order.getOrderNumber());
+            System.out.println("***********************************************************\n");
+            System.out.println("Order Placed: " + order.getFormattedOrderDate());
+            System.out.println("Amazon.com order number: " + order.getOrderNumber());
+            System.out.println("Order Total: " + order.getFormattedItemsSubtotal() + "\n");
+            System.out.println("Shipped on " + order.shipment.getFormattedShippedDate() + "\n");
+            System.out.println("Items Ordered/Price\n ");
+            for (OrderItem item : order.orderItems) {
+                System.out.println(item.product.productDescription);
+                System.out.println("Quantity: " + item.quantity);
+                System.out.println("Sold by: " + item.product.soldBy);
+                System.out.println("Price : " + item.product.price);
+                System.out.println("Condition: " + item.product.condition + "\n");
+            }
+            System.out.println("Shipping Address\n");
+            System.out.println(order.getCustomer().getCustomerName());
+            System.out.println(order.getCustomer().getStressAddress());
+            System.out.println(order.getCustomer().getCityStateZip());
+            System.out.println(order.getCustomer().getCountry() + "\n");
+            System.out.println("Shipping Speed:");
+            System.out.println(order.shipment.shipmentSpeed);
+            System.out.println("Payment Method:");
+            System.out.println("Signature | " + order.payment.getPartialAccountNumber());
+            System.out.println("Rewards Points\n");
+            System.out.print("Item(s) Subtotal: ");
+            System.out.println(order.getFormattedItemsSubtotal());
+            System.out.print("Shipping and Handling: ");
+            System.out.println(order.getShippingHandlingFormatted() + "\n");
+            System.out.print("Total before tax:  ");
+            System.out.println(order.getTotalBeforeTaxes());
+            System.out.print("Estimated tax to be collected: ");
+            System.out.println(order.getFormattedEstimatedTaxes() + "\n");
+            System.out.print("Grand Total: ");
+            System.out.println(order.getFormattedGrandTotal() + "\n");
+            System.out.println("Billing Address\n");
+            System.out.println(order.getCustomer().getCustomerName());
+            System.out.println(order.getCustomer().getStressAddress());
+            System.out.println(order.getCustomer().getCityStateZip());
+            System.out.println(order.getCustomer().getCountry() + "\n");
+            System.out.println("Credit Card Transactions: ");
+            System.out.println(order.payment.getBankOrIssuer() + " ending in " + order.payment.getPartialAccountNumber() + ": " + order.payment.getFormattedPaymentDate() + ": " + order.payment.getFormattedPaymentAmount());
+        }
+
+
 
 }
